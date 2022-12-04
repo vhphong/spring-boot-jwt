41c42e45493a5541b98b5c9de70814275854797e
vhphong <vhphong@gmail.com>
2022-12-04 17:13:02 -0500

    remodeled

--- src/main/java/com/phongvo/springbootjwt/SpringBootJwtApplication.java
+++ src/main/java/com/phongvo/springbootjwt/SpringBootJwtApplication.java
@@ -1,16 +1,7 @@
 package com.phongvo.springbootjwt;
 
-import com.phongvo.springbootjwt.models.Account;
-import com.phongvo.springbootjwt.models.Role;
-import com.phongvo.springbootjwt.services.AccountService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class SpringBootJwtApplication {
@@ -19,32 +10,5 @@ public class SpringBootJwtApplication {
         SpringApplication.run(SpringBootJwtApplication.class, args);
     }
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    CommandLineRunner run(AccountService accountService) {
-        return args -> {
-            accountService.saveRole((new Role(null, "ROLE_USER")));
-            accountService.saveRole((new Role(null, "ROLE_MANAGER")));
-            accountService.saveRole((new Role(null, "ROLE_ADMIN")));
-            accountService.saveRole((new Role(null, "ROLE_SUPER_ADMIN")));
-
-            accountService.saveAccount(new Account(null, "Phong Vo", "phong", "1234", new ArrayList<>()));
-            accountService.saveAccount(new Account(null, "Turing", "turing", "1234", new ArrayList<>()));
-            accountService.saveAccount(new Account(null, "Nicholas Cage", "nicholas", "1234", new ArrayList<>()));
-            accountService.saveAccount(new Account(null, "Mark Wahlberg", "mark", "1234", new ArrayList<>()));
-
-            accountService.addRoleToAccount("phong", "ROLE_SUPER_ADMIN");
-            accountService.addRoleToAccount("turing", "ROLE_ADMIN");
-            accountService.addRoleToAccount("nicholas", "ROLE_ADMIN");
-            accountService.addRoleToAccount("nicholas", "ROLE_MANAGER");
-            accountService.addRoleToAccount("mark", "ROLE_USER");
-            accountService.addRoleToAccount("mark", "ROLE_MANAGER");
-            accountService.addRoleToAccount("mark", "ROLE_ADMIN");
-        };
-    }
 
 }
---
+++ src/main/java/com/phongvo/springbootjwt/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.phongvo.springbootjwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager user() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("phongvo")
+                        .password("1234")
+                        .authorities("read")
+                        .build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(auth -> auth.anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
